9dc264642aa83235c81ecf1b330e16d5b7c1dd88
Adam Borys <adamborys2002@wp.pl>
2021-10-18 15:09:36 +0200

    Problem

--- wizytowki.py
+++ wizytowki.py
@@ -21,20 +21,31 @@ class Card:
         return (len(self.name) - 1)
 
 
-
-
-
 person_1 = Card(name=fake.name(), surrname=fake.name(), company=fake.company(), position=fake.job(), email=fake.email())
-person_2 = Card(name=fake.name(), surrname=fake.name(), company=fake.company(), position=fake.job(), email=fake.email())
-person_3 = Card(name=fake.name(), surrname=fake.name(), company=fake.company(), position=fake.job(), email=fake.email())
-person_4 = Card(name=fake.name(), surrname=fake.name(), company=fake.company(), position=fake.job(), email=fake.email())
-person_5 = Card(name=fake.name(), surrname=fake.name(), company=fake.company(), position=fake.job(), email=fake.email())
-
-person_list = [person_1, person_2, person_3, person_4, person_5]
-card_list = []
-
-Card.contact(person_1)
 
 
 
 
+class BaseContact:
+    def __init__(self, name, phone_number, email):
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+    def contact(self):
+        return f"Wybieram numer {self.phone_number} i dzwonię do {self.name}"
+
+
+class BusinessContact(BaseContact):
+    def __init__(self, position, company, business_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.position = position
+        self.company = company
+        self.business_phone = business_phone
+    def contact(self):
+        return f"Wybieram numer {self.business_phone} i dzwonię do {self.name}"
+
+
+person_2 = BaseContact(name=fake.name(), phone_number=fake.phone_number(), email=fake.email())
+person_3 = BusinessContact(name=fake.name(), phone_number=fake.phone_number(), email=fake.email(), position=fake.job(), company=fake.company(), business_phone=fake.phone_number())
+
+contact(person_2)
