21b8ce99907dbcf3d0db1875a1999181cba8002a
Adam Borys <adamborys2002@wp.pl>
2021-10-18 22:31:26 +0200

    Drugi

--- biblioteka_film.py
+++ biblioteka_film.py
@@ -5,6 +5,12 @@ class Film:
         self.genre = genre
         self.number_of_plays = number_of_plays
 
+    def play(self):
+        self.number_of_plays += 1
+
+    def __str__(self):
+        print(f"{self.title} ({self.year})")
+
 
 class Series:
     def __init__(self, title, year, genre, episode, season, number_of_plays):
@@ -15,3 +21,14 @@ class Series:
         self.season = season
         self.number_of_plays = number_of_plays
 
+    def play(self):
+        self.number_of_plays += 1
+
+    def __str__(self):
+        print(f"{self.title} S{self.season}E{self.episode}")
+
+simpsons = Series(title="The Simpsons", year="1978", genre="Comedy", episode="03", season="02", number_of_plays=320)
+pulp_fiction = Film(title="Pulp Fiction", year="1994", genre="Crime", number_of_plays=472)
+
+simpsons.__str__()
+pulp_fiction.__str__()
