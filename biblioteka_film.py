017db3ea9cf9525414e4fc896eff17239f52801e
Adam Borys <adamborys2002@wp.pl>
2021-10-18 22:13:41 +0200

    Pierwszy

---
+++ biblioteka_film.py
@@ -0,0 +1,17 @@
+class Film:
+    def __init__(self, title, year, genre, number_of_plays):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.number_of_plays = number_of_plays
+
+
+class Series:
+    def __init__(self, title, year, genre, episode, season, number_of_plays):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.episode = episode
+        self.season = season
+        self.number_of_plays = number_of_plays
+
